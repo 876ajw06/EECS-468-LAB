bae8a809f12762319851a25a9580d31edd6989df
alexanderwichern <alexjw7777@gmail.com>
2026-01-30 13:35:08 -0600

    printf
    int main() {
    
    char str[100];
    
    int i, len;
    
    printf("Enter a string: ");
    
    gets(str);
    
    len = strlen(str);
    
    for (i = 0; i < len; i++) {
    
    if (str[i] >= 'a' && str[i] <= 'z') {
    
    str[i] = str[i] -
    
    'a' + 'A';
    
    }
    
    }
    
    printf("The capitalized string is: %s\n", str);
    
    // Reverse the string.
    
    for (i = 0, len = strlen(str); i < len / 2; i++) {
    
    char temp = str[i];
    
    str[i] = str[len -
    
    int main() {
    
    char str[100];
    
    int i, len;
    
    printf("Enter a string: ");
    
    gets(str);
    
    len = strlen(str);
    
    for (i = 0; i < len; i++) {
    
    if (str[i] >= 'a' && str[i] <= 'z') {
    
    str[i] = str[i] -
    
    'a' + 'A';
    
    }
    
    }
    
    printf("The capitalized string is: %s\n", str);
    
    // Reverse the string.
    
    for (i = 0, len = strlen(str); i < len / 2; i++) {
    
    char temp = str[i];
    
    str[i] = str[len -
    
    1 - i];
    
    str[len - 1 - i] = temp;
    
    }
    
    printf("The reversed string is: %s\n", str);
    
    return 0;
    
    }1 - i];
    
    str[len - 1 - i] = temp;
    
    }
    
    printf("The reversed string is: %s\n", str);
    
    return 0;
    
    }#	.viminfo

--- helloworld.c
+++ helloworld.c
@@ -1,16 +1,30 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-    printf("Bye World");
-
-    return 0;
+int main() {
+	char str[100];
+	int i, len;
+	
+	printf("Enter a string: ");
+	gets(str);
+	
+	len = strlen(str);
+	
+	for (i = 0; i < len; i++) {
+		if (str[i] >= 'a' && str[i] <= 'z') {
+			str[i] = str[i] - 'a' + 'A';
+		}
+	}
+	
+	printf("The capitalized string is: %s\n", str);
+	
+	// Reverse the string.
+	for (i = 0, len = strlen(str); i < len / 2; i++) {
+		char temp = str[i];
+		str[i] = str[len - 1 - i];
+		str[len - 1 - i] = temp;
+	}
+	
+	printf("The reversed string is: %s\n", str);
+	return 0;
 }
